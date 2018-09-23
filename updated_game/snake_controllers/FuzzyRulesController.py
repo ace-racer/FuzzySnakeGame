e0fb14d542b6d2f008d586a23a58f63bfc08906e
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-09-23 15:35:50 +0800

    Add move counter and update one rule

--- updated_game/snake_controllers/FuzzyRulesController.py
+++ updated_game/snake_controllers/FuzzyRulesController.py
@@ -1,6 +1,17 @@
+from Game import Game
+import constants
+
 class FuzzyRulesController:
-    def __init__(self):
-        pass
+    def __init__(self, max_moves=1000):
+        self.max_moves = max_moves
+        self.current_move_number = 0
+        self.game = Game()
 
     def perform_next_move(self, snake, food, bricks):
-        return snake, True
+        if self.current_move_number < self.max_moves:
+            
+            # Fuzzy rules to be put here!
+
+            self.current_move_number += 1
+            return snake, True
+        return snake, False
--- updated_game/snake_controllers/RuleBasedController.py
+++ updated_game/snake_controllers/RuleBasedController.py
@@ -2,20 +2,20 @@ from Game import Game
 import constants
 
 class RuleBasedController:
-    def __init__(self, max_moves=100):
+    def __init__(self, max_moves=1000):
         self.max_moves = max_moves
-        self.current_move = 0
+        self.current_move_number = 0
         self.game = Game()
 
     def perform_next_move(self, snake, food, bricks):
-        if self.current_move < self.max_moves:
+        if self.current_move_number < self.max_moves:
             current_snake_direction = snake.getCurrentDirection()
 
             # if the food is to the left of the snake
             if food.x < snake.x[0]:
                 snake.moveLeft()
             # if food is to the right of the snake
-            elif food.x > snake.x[0]:
+            elif food.x > snake.x[0] and current_snake_direction != constants.LEFT:
                 snake.moveRight()
             else:
                 # the head of snake is at same column as the food
@@ -26,7 +26,8 @@ class RuleBasedController:
                 # snake is higher than the food
                 elif food.y > snake.y[0]:
                     snake.moveDown()
-
+            
+            self.current_move_number += 1
             return snake, True
         return snake, False
 
