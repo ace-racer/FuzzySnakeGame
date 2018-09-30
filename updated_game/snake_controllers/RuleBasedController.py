658b2eb34f39e6facb9e7da91ddb6b1a4656c1fc
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-09-30 20:29:55 +0800

    Add else conditions to keep snake alive

--- updated_game/snake_controllers/RuleBasedController.py
+++ updated_game/snake_controllers/RuleBasedController.py
@@ -17,11 +17,15 @@ class RuleBasedController:
                 distance_from_wall = snake.get_distance_from_wall(bricks, constants.LEFT)
                 if not self.is_distance_too_less(distance_from_wall):
                     snake.moveLeft()
+                else:
+                    snake.moveDown()
             # if food is to the right of the snake
             elif food.x > snake.x[0] and current_snake_direction != constants.LEFT:
                 distance_from_wall = snake.get_distance_from_wall(bricks, constants.RIGHT)
                 if not self.is_distance_too_less(distance_from_wall):
                     snake.moveRight()
+                else:
+                    snake.moveUp()
             else:
                 # the head of snake is at same column as the food
             
@@ -30,18 +34,22 @@ class RuleBasedController:
                     distance_from_wall = snake.get_distance_from_wall(bricks, constants.UP)
                     if not self.is_distance_too_less(distance_from_wall):
                         snake.moveUp()
+                    else:
+                        snake.moveLeft()
                 # snake is higher than the food
                 elif food.y > snake.y[0] and current_snake_direction != constants.UP:
                     distance_from_wall = snake.get_distance_from_wall(bricks, constants.DOWN)
                     if not self.is_distance_too_less(distance_from_wall):
                         snake.moveDown()
+                    else:
+                        snake.moveRight()
             
             self.current_move_number += 1
             return snake, True
         return snake, False
 
     def is_distance_too_less(self, distance):
-        if distance <= constants.STEP_SIZE * 2:
+        if distance <= (constants.STEP_SIZE * 2):
             return True
         return False
 
