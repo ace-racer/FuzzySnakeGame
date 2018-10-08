a49c71007af15b817ca19a88e5ea62a38c70e582
Ken <gohkenrick@gmail.com>
2018-10-08 10:42:36 +0800

    Updated ( Not Working )
    TypeError: get_angle_pm_right() missing 4 required positional arguments: 'food_y', 'food_x', 'snake_y', and 'snake_x'

--- updated_game/snake_controllers/FuzzyRulesController.py
+++ updated_game/snake_controllers/FuzzyRulesController.py
@@ -11,6 +11,8 @@ import math as m
 #myradians = math.atan2(targetY-gunY, targetX-gunX)
 
 class FuzzyRulesController:
+
+
     def __init__(self, max_moves=1000):
         self.max_moves = max_moves
         self.current_move_number = 0
@@ -19,73 +21,115 @@ class FuzzyRulesController:
         self.mf_pm_left()
         self.mf_pm_up()
         self.mf_pm_down()
+        self.get_angle_pm_right()
+        self.get_angle_pm_left()
+        self.get_angle_pm_up()
+        self.get_angle_pm_down()
 
 
     def mf_pm_right(self):
-        # New Antecedent/Consequent objects hold universe variables and membership functions
-        # pm_right = ctrl.Antecedent(np.arange(0, 101, 1), 'pm_right')
-        # pm_left = ctrl.Antecedent(np.arange(0, 101, 1), 'pm_left')
-        # pm_up = ctrl.Antecedent(np.arange(0, 101, 1), 'pm_up')
-        # pm_down = ctrl.Antecedent(np.arange(0, 101, 1), 'pm_down')
         '''if the previous move is RIGHT, the only direciton
         you can move is UP, RIGHT, DOWN'''
-        next_move_pm_right = ctrl.Consequent(np.arange(0, 101, 1), 'Next Direction')
-        next_move_pm_right['up'] = fuzz.trimf(next_move_pm_right.universe,[0,25, 50])
-        next_move_pm_right['right'] = fuzz.trimf(next_move_pm_right.universe,[25,50,75])
-        next_move_pm_right['down'] = fuzz.trimf(next_move_pm_right.universe,[50,75,100])
-        food_loc = ctrl.Antecedent(np.arange(-180, 181, 1), 'food_loc')
-        '''Angle is based on the 4 quadrant'''
+        self.next_move_pm_right = ctrl.Consequent(np.arange(0, 101, 1), 'Next Direction')
+        self.next_move_pm_right['up'] = fuzz.trimf(self.next_move_pm_right.universe,[0,25, 50])
+        self.next_move_pm_right['right'] = fuzz.trimf(self.next_move_pm_right.universe,[25,50,75])
+        self.next_move_pm_right['down'] = fuzz.trimf(self.next_move_pm_right.universe,[50,75,100])
 
-        food_loc['up'] = fuzz.trimf(food_loc.universe,[-90,0, 90])
-        food_loc['left'] = fuzz.trimf(food_loc.universe,[45,135,180])
-        food_loc['right'] = fuzz.trimf(food_loc.universe, [-180,-135,-45])
+        self.food_loc = ctrl.Antecedent(np.arange(-180, 181, 1), 'food_loc')
+        '''Based on a clock with respect the the snake head'''
+        self.food_loc['up'] = fuzz.trimf(self.food_loc.universe,[-90,0, 90])
+        self.food_loc['left'] = fuzz.trimf(self.food_loc.universe,[45,135,180])
+        self.food_loc['right'] = fuzz.trimf(self.food_loc.universe, [-180,-135,-45])
+
+        self.rule1 = ctrl.Rule(self.food_loc['up'], self.next_move_pm_right['right'])
+        self.rule2 = ctrl.Rule(self.food_loc['left'], self.next_move_pm_right['up'])
+        self.rule3 = ctrl.Rule(self.food_loc['right'], self.next_move_pm_right['down'])
 
     def mf_pm_left(self):
         '''if the previous move is LEFT, the only direciton
         you can move is UP, LEFT, DOWN'''
-        next_move_pm_left = ctrl.Consequent(np.arange(0, 101, 1), 'Next Direction')
-        next_move_pm_left['up'] = fuzz.trimf(next_move_pm_left.universe,[0,25, 50])
-        next_move_pm_left['left'] = fuzz.trimf(next_move_pm_left.universe,[25,50,75])
-        next_move_pm_left['down'] = fuzz.trimf(next_move_pm_left.universe,[50,75,100])
+        self.next_move_pm_left = ctrl.Consequent(np.arange(0, 101, 1), 'Next Direction')
+        self.next_move_pm_left['up'] = fuzz.trimf(self.next_move_pm_left.universe,[0,25, 50])
+        self.next_move_pm_left['left'] = fuzz.trimf(self.next_move_pm_left.universe,[25,50,75])
+        self.next_move_pm_left['down'] = fuzz.trimf(self.next_move_pm_left.universe,[50,75,100])
 
-        food_loc = ctrl.Antecedent(np.arange(-180, 181, 1), 'food_loc')
-        '''Based on a clock'''
+        self.food_loc = ctrl.Antecedent(np.arange(-180, 181, 1), 'food_loc')
+        '''Based on a clock with respect the the snake head'''
+        self.food_loc['up'] = fuzz.trimf(self.food_loc.universe,[-90,0, 90])
+        self.food_loc['left'] = fuzz.trimf(self.food_loc.universe,[45,135,180])
+        self.food_loc['right'] = fuzz.trimf(self.food_loc.universe, [-180,-135,-45])
 
-        food_loc['up'] = fuzz.trimf(food_loc.universe,[-90,0, 90])
-        food_loc['left'] = fuzz.trimf(food_loc.universe,[45,135,180])
-        food_loc['right'] = fuzz.trimf(food_loc.universe, [-180,-135,-45])
+        self.rule1 = ctrl.Rule(self.food_loc['up'], self.next_move_pm_left['left'])
+        self.rule2 = ctrl.Rule(self.food_loc['left'], self.next_move_pm_left['up'])
+        self.rule3 = ctrl.Rule(self.food_loc['right'], self.next_move_pm_left['down'])
+
 
     def mf_pm_up(self):
         '''if the previous move is UP, the only direciton
         you can move is UP, LEFT, RIGHT'''
-        next_move_pm_up = ctrl.Consequent(np.arange(0, 101, 1), 'Next Direction')
-        next_move_pm_up['left'] = fuzz.trimf(next_move_pm_up.universe,[0,25, 50])
-        next_move_pm_up['up'] = fuzz.trimf(next_move_pm_up.universe,[25,50,75])
-        next_move_pm_up['down'] = fuzz.trimf(next_move_pm_up.universe, [50,75,100])
-        food_loc = ctrl.Antecedent(np.arange(-180, 181, 1), 'food_loc')
-        '''Angle is based on the 4 quadrant'''
+        self.next_move_pm_up = ctrl.Consequent(np.arange(0, 101, 1), 'Next Direction')
+        self.next_move_pm_up['left'] = fuzz.trimf(self.next_move_pm_up.universe,[0,25, 50])
+        self.next_move_pm_up['up'] = fuzz.trimf(self.next_move_pm_up.universe,[25,50,75])
+        self.next_move_pm_up['right'] = fuzz.trimf(self.next_move_pm_up.universe, [50,75,100])
 
-        food_loc['up'] = fuzz.trimf(food_loc.universe,[-90,0, 90])
-        food_loc['left'] = fuzz.trimf(food_loc.universe,[45,135,180])
-        food_loc['right'] = fuzz.trimf(food_loc.universe, [-180,-135,-45])
+        self.food_loc = ctrl.Antecedent(np.arange(-180, 181, 1), 'food_loc')
+        '''Based on a clock with respect the the snake head'''
+        self.food_loc['up'] = fuzz.trimf(self.food_loc.universe,[-90,0, 90])
+        self.food_loc['left'] = fuzz.trimf(self.food_loc.universe,[45,135,180])
+        self.food_loc['right'] = fuzz.trimf(self.food_loc.universe, [-180,-135,-45])
 
+        self.rule1 = ctrl.Rule(self.food_loc['up'], self.next_move_pm_up['up'])
+        self.rule2 = ctrl.Rule(self.food_loc['left'], self.next_move_pm_up['left'])
+        self.rule3 = ctrl.Rule(self.food_loc['right'], self.next_move_pm_up['right'])
+    #
     def mf_pm_down(self):
         '''if the previous move is DOWN, the only direciton
         you can move is DOWN, LEFT, RIGHT'''
-        next_move_pm_down = ctrl.Consequent(np.arange(0, 101, 1), 'Next Direction')
-        next_move_pm_down['left'] = fuzz.trimf(next_move_pm_down.universe,[0,25, 50])
-        next_move_pm_down['down'] = fuzz.trimf(next_move_pm_down.universe,[25,50,75])
-        next_move_pm_down['right'] = fuzz.trimf(next_move_pm_down.universe,[50,75,100])
+        self.next_move_pm_down = ctrl.Consequent(np.arange(0, 101, 1), 'Next Direction')
+        self.next_move_pm_down['left'] = fuzz.trimf(self.next_move_pm_down.universe,[0,25, 50])
+        self.next_move_pm_down['down'] = fuzz.trimf(self.next_move_pm_down.universe,[25,50,75])
+        self.next_move_pm_down['right'] = fuzz.trimf(self.next_move_pm_down.universe,[50,75,100])
 
-        #Brick Location
-        food_loc = ctrl.Antecedent(np.arange(-180, 181, 1), 'food_loc')
-        '''Angle is based on the 4 quadrant'''
+        self.food_loc = ctrl.Antecedent(np.arange(-180, 181, 1), 'food_loc')
+        '''Based on a clock with respect the the snake head'''
+        self.food_loc['up'] = fuzz.trimf(self.food_loc.universe,[-90,0, 90])
+        self.food_loc['left'] = fuzz.trimf(self.food_loc.universe,[45,135,180])
+        self.food_loc['right'] = fuzz.trimf(self.food_loc.universe, [-180,-135,-45])
 
-        food_loc['up'] = fuzz.trimf(food_loc.universe,[-90,0, 90])
-        food_loc['left'] = fuzz.trimf(food_loc.universe,[45,135,180])
-        food_loc['right'] = fuzz.trimf(food_loc.universe, [-180,-135,-45])
+        self.rule1 = ctrl.Rule(self.food_loc['up'], self.next_move_pm_down['down'])
+        self.rule2 = ctrl.Rule(self.food_loc['left'], self.next_move_pm_down['right'])
+        self.rule3 = ctrl.Rule(self.food_loc['right'], self.next_move_pm_down['left'])
 
-    def get_angle(food_y, food_x, snake_y,snake_x):
+
+    def get_angle_pm_right(self,food_y, food_x, snake_y,snake_x):
+        '''Angle is based on the 4 quadrant
+        We require to convert it back where 0 is the y-axis'''
+        angle = m.degrees(m.atan2((food_y-snake_y),(food_x - snake_x)))
+        if angle <= 90 and angle >= 0:
+            final_angle = -angle
+        if angle >= 90 and angle <= 180:
+            final_angle = -angle
+        if  -90 <=angle <= 0:
+            final_angle = -angle
+        if angle >= -180 and angle <= -90:
+            final_angle = -angle
+        return int(final_angle)
+
+    def get_angle_pm_left(self,food_y, food_x, snake_y,snake_x):
+        '''Angle is based on the 4 quadrant
+        We require to convert it back where 0 is the y-axis'''
+        angle = m.degrees(m.atan2((food_y-snake_y),(food_x - snake_x)))
+        if angle <= 90 and angle >= 0:
+            final_angle = 180-angle
+        if angle >= 90 and angle <= 180:
+            final_angle = 180-angle
+        if  -90 <=angle <= 0:
+            final_angle = -180 - angle
+        if angle >= -180 and angle <= -90:
+            final_angle = -180 - angle
+        return int(final_angle)
+
+    def get_angle_pm_up(self,food_y, food_x, snake_y,snake_x):
         '''Angle is based on the 4 quadrant
         We require to convert it back where 0 is the y-axis'''
         angle = m.degrees(m.atan2((food_y-snake_y),(food_x - snake_x)))
@@ -97,101 +141,78 @@ class FuzzyRulesController:
             final_angle = 90 -angle
         if angle >= -180 and angle <= -90:
             final_angle = (180 + angle) + 90
-            return int(final_angle)
-            # m.degress(m.atan2((food.y-snake.y[0]),(food.x -snake.x[0])))
+        return int(final_angle)
 
-
-        # m.degress(m.atan2((food.y-snake.y[0]),(food.x -snake.x[0])))
+    def get_angle_pm_down(self,food_y, food_x, snake_y,snake_x):
+        '''Angle is based on the 4 quadrant
+        We require to convert it back where 0 is the y-axis'''
+        angle = m.degrees(m.atan2((food_y-snake_y),(food_x - snake_x)))
+        if angle <= 90 and angle >= 0:
+            final_angle = -90 - angle
+        if angle >= 90 and angle <= 180:
+            final_angle = 180 - angle + 90
+        if  -90 <=angle <= 0:
+            final_angle = -(90-angle)
+        if angle >= -180 and angle <= -90:
+            final_angle = -angle -90
+        return int(final_angle)
 
 
     def perform_next_move(self, snake, food, bricks):
         if self.current_move_number < self.max_moves:
             if snake.direction == constants.RIGHT:
                 self.mf_pm_right()
-
-                rule1 = ctrl.Rule(mf_pm_right.food_loc['up'], next_move['up'])
-                rule2 = ctrl.Rule(mf_pm_right.food_loc['left'], next_move['left'])
-                rule3 = ctrl.Rule(mf_pm_right.food_loc['right'], next_move['right'])
-
-                angle = get_angle(food.y, food.x, snake.y[0], snake.x[0])
-                next_move_crtl = ctrl.ControlSystem([rule1, rule2, rule3])
+                angle = self.get_angle_pm_right(food.y, food.x, snake.y[0], snake.x[0])
+                print(angle)
+                next_move_crtl = ctrl.ControlSystem([self.rule1, self.rule2, self.rule3])
                 next_move_crtl_fuzzy = ctrl.ControlSystemSimulation(next_move_crtl)
-
                 next_move_crtl_fuzzy.input['food_loc'] = angle
                 result = next_move_crtl_fuzzy.compute()
-                if result < 3:
+                result = int(next_move_crtl_fuzzy.output['Next Direction'])
+                print(str(result)+"lulz" )
+                if result < 33:
                     snake.moveUp()
-                if result >=3 and result <=7:
+                if result >=33 and result <=67:
                     snake.moveRight()
                 else:
                     snake.moveDown()
+
             if snake.direction == constants.LEFT:
                 self.mf_pm_left()
-
-                rule1 = ctrl.Rule(food_loc['up'], next_move['up'])
-                rule2 = ctrl.Rule(food_loc['left'], next_move['left'])
-                rule3 = ctrl.Rule(food_loc['down'], next_move['down'])
-
-                angle = get_angle(food.y, food.x, snake.y[0], snake.x[0])
-                next_move_crtl = ctrl.ControlSystem([rule1, rule2, rule3])
+                angle = self.get_angle_pm_left(food.y, food.x, snake.y[0], snake.x[0])
+                print(angle)
+                next_move_crtl = ctrl.ControlSystem([self.rule1, self.rule2, self.rule3])
                 next_move_crtl_fuzzy = ctrl.ControlSystemSimulation(next_move_crtl)
-
                 next_move_crtl_fuzzy.input['food_loc'] = angle
                 result = next_move_crtl_fuzzy.compute()
-                if result < 3:
+                result = int(next_move_crtl_fuzzy.output['Next Direction'])
+                print(str(result)+"lulz" )
+                if result < 33:
                     snake.moveUp()
-                if result >=3 and result <=7:
+                if result >=33 and result <=67:
                     snake.moveLeft()
                 else:
                     snake.moveDown()
 
             if snake.direction == constants.UP:
-                self.mf_pm_Up()
-
-                rule1 = ctrl.Rule(food_loc['left'], next_move['left'])
-                rule2 = ctrl.Rule(food_loc['up'], next_move['up'])
-                rule3 = ctrl.Rule(food_loc['down'], next_move['down'])
-
-                angle = get_angle(food.y, food.x, snake.y[0], snake.x[0])
-                next_move_crtl = ctrl.ControlSystem([rule1, rule2, rule3])
+                self.mf_pm_up()
+                angle = self.get_angle_pm_up(food.y, food.x, snake.y[0], snake.x[0])
+                print(angle)
+                next_move_crtl = ctrl.ControlSystem([self.rule1, self.rule2, self.rule3])
                 next_move_crtl_fuzzy = ctrl.ControlSystemSimulation(next_move_crtl)
-
                 next_move_crtl_fuzzy.input['food_loc'] = angle
                 result = next_move_crtl_fuzzy.compute()
-                if result < 3:
+                result = int(next_move_crtl_fuzzy.output['Next Direction'])
+                print(str(result)+"lulz" )
+                if result < 33:
                     snake.moveLeft()
-                if result >=3 and result <=7:
+                if result >=33 and result <=67:
                     snake.moveUp()
                 else:
-                    snake.moveDown()
-            if snake.direction == constants.DOWN:
-                self.mf_pm_Down()
+                    snake.moveRight()
 
-                rule1 = ctrl.Rule(food_loc['up'], next_move['up'])
-                rule2 = ctrl.Rule(food_loc['left'], next_move['left'])
-                rule3 = ctrl.Rule(food_loc['right'], next_move['right'])
-
-                angle = get_angle(food.y, food.x, snake.y[0], snake.x[0])
-                next_move_crtl = ctrl.ControlSystem([rule1, rule2, rule3])
-                next_move_crtl_fuzzy = ctrl.ControlSystemSimulation(next_move_crtl)
-
-                next_move_crtl_fuzzy.input['food_loc'] = angle
-                result = next_move_crtl_fuzzy.compute()
-                if result < 3:
-                    snake.moveUp()
-                if result >=3 and result <=7:
-                    snake.moveLeft()
-                else:
-                    snake.moveDown()
-
-            # if (next_move_crtl_fuzzy.output['score'])
-
-            #Propose 1 fuzzy system for each direction
-            # if self.direction == constants.RIGHT:
-
-
-            # Fuzzy rules to be put here!
 
             self.current_move_number += 1
             return snake, True
         return snake, False
+
