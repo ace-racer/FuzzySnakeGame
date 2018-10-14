b15e427b8931f6beadb6b70bda5a27cd316f9a32
Anurag Chatterjee <anuragchatterjee92@gmail.com>
2018-10-14 19:15:59 +0800

    Remove commented code

--- updated_game/Game.py
+++ updated_game/Game.py
@@ -1,34 +1,7 @@
-# class Game:
-#     def isCollision(self,x1,y1,x2,y2,step_size):
-#         # top left corner (x1,y1) inside the square formed by x2,y2
-#         # top right corner (x1 + step_size,y1) inside the square formed by x2,y2
-#
-#         corners = [(x1, y1), (x1 + step_size, y1), (x1, y1 + step_size), (x1 + step_size, y1 + step_size)]
-#
-#         for corner in corners:
-#             if corner[0] >= x2 and corner[0] <= x2 + step_size:
-#                 if corner[1] >= y2 and corner[1] <= y2 + step_size:
-#                     return True
-#
-#
-#
-#         return False
-
 class Game:
     def isCollision(self,x1,y1,x2,y2,step_size):
-        # top left corner (x1,y1) inside the square formed by x2,y2
-        # top right corner (x1 + step_size,y1) inside the square formed by x2,y2
-
-        # corners = [(x1, y1), (x1 + step_size, y1), (x1, y1 + step_size), (x1 + step_size, y1 + step_size)]
 
         if x1 == x2 and y1 == y2:
             return True
-
-        # for corner in corners:
-        #     if corner[0] >= x2 and corner[0] <= x2 + step_size:
-        #         if corner[1] >= y2 and corner[1] <= y2 + step_size:
-        #             return True
-
-
 
         return False
